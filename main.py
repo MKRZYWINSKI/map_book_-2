2e29271aae6fbeca6986475b716bbd7331c7ea4d
MKRZYWINSKI <u56106@student.wat.edu.pl>
2024-05-28 22:43:16 +0200

    wip:add a menu

--- main.py
+++ main.py
@@ -9,7 +9,18 @@ users: list[dict] = [
 
 def read_users(user_list: list[dict]) -> None:
     for user in user_list:
-        [print(f"Twój znajomy {user['name']}: {user['surname']}, opublikował{user['posts']} postów")]
+        print(f"Twój znajomy {user['name']}: {user['surname']}, opublikował{user['posts']} postów")
 
 
-read_users(users)
+
+if __name__ == "__main__":
+     print("Witaj użytkowniku")
+     while True:
+         print("Menu:")
+         print("1.Wyswietl co u znajomych")
+         manu_option: str = input("Dokonaj wyboru")
+         if  manu_option == "0":
+             print("Program kończy prace")
+             break
+         if  manu_option == "1":
+                read_users(users)
