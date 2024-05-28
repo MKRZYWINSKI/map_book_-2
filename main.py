54e8f2ea90035a380dbbbf321d3c276cb4d26bc9
MKRZYWINSKI <u56106@student.wat.edu.pl>
2024-05-28 21:42:47 +0200

    ADD:Add users print functionality

---
+++ main.py
@@ -0,0 +1,18 @@
+imie="Michał"
+nazwisko="Lębryk"
+liczba_postow=233
+
+print(f"Twój znajomy {imie}{nazwisko} opublikował {liczba_postow} postów")
+
+imie_2="Kacper"
+nazwisko_2="Macioch"
+liczba_postow_2=9
+
+print(f"Twój znajomy {imie_2}{nazwisko_2} opublikował {liczba_postow_2} postów")
+
+imie_3="Jakub"
+nazwisko_3="Łojek"
+liczba_postow_3=9
+
+print(f"Twój znajomy {imie_3}{nazwisko_3} opublikował {liczba_postow_3} postów")
+
