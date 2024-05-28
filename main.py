b9b776a6a96cbdc12798e38967c941abff9ae2b7
MKRZYWINSKI <u56106@student.wat.edu.pl>
2024-05-28 22:01:19 +0200

    ADD:prepare a for loop

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
-imie="Michał"
-nazwisko="Lębryk"
-liczba_postow=233
+users: list[dict] = [
+    {"name":"Kacper", "surname":"Macioch", "posts": 999 },
+    {"name":"Michał", "surname":"Lębryk", "posts": 979 },
+    {"name":"Dominik", "surname":"Kużnik", "posts": 989 },
+    {"name":"Leon", "surname":"Hajdus", "posts": 229 }
 
-print(f"Twój znajomy {imie}{nazwisko} opublikował {liczba_postow} postów")
-
-imie_2="Kacper"
-nazwisko_2="Macioch"
-liczba_postow_2=9
-
-print(f"Twój znajomy {imie_2}{nazwisko_2} opublikował {liczba_postow_2} postów")
-
-imie_3="Jakub"
-nazwisko_3="Łojek"
-liczba_postow_3=9
-
-print(f"Twój znajomy {imie_3}{nazwisko_3} opublikował {liczba_postow_3} postów")
-
+]
+for user in users:
+     [print(f"Twój znajomy {user['name']}: {user['surname']}, opublikował{user['posts']} postów")]
