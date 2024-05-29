41f8cc8502eb2073b511ca6c206bd1557b06813e
MKRZYWINSKI <u56106@student.wat.edu.pl>
2024-05-29 08:28:28 +0200

    wip: add a functionality to create new user and added ę

--- utils/crud.py
+++ utils/crud.py
@@ -1,3 +1,11 @@
 def read_users(user_list: list[dict]) -> None:
     for user in user_list:
         print(f"Twój znajomy {user['name']}: {user['surname']}, opublikował{user['posts']} postów")
+
+def add_user(users: list)-> None:
+    new_name: str = input("Podaj imie:")
+    new_surname: str = input("Podaj imie:")
+    new_posts: int = int(input("Podaj liczbe punktów"))
+    new_user: dict = {"name": new_name, "surname": new_surname, "posts": new_posts}
+    print(new_user)
+    users.append(new_user)
